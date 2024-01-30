21ebe6bbe0c6c3add116dab5def7819da4b9f1e6
meghnabajoria <meghnabajoria8@gmail.com>
2024-01-30 12:50:00 -0800

    Added code to return keywords in server.py

--- src/server.py
+++ src/server.py
@@ -1,6 +1,26 @@
 from flask import Flask, request, jsonify
+import textwrap
+import google.generativeai as genai
+from IPython.display import Markdown
 
 app = Flask(__name__)
+
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+def generate_content(job_description):
+   genai.configure(api_key="insert your api key")
+   model = genai.GenerativeModel('gemini-pro')
+   response = model.generate_content("""I have the following Job Description:
+                {document}
+                Based on the above job description, extract technical keywords that best describe the 
+                skillsets and technologies required for the above job. Pay special attention to programming 
+                languages, tools, and technologies mentioned.
+                Use the following format separated by commas:
+                <keywords>
+                """)
+   to_markdown(response.text)
 
 @app.route('/', methods = ['GET','POST'])
 def process_request():
@@ -12,8 +32,9 @@ def process_request():
     processed_text = selected_text.upper()
 
     print("Processed Text: ", processed_text)
+    markdown_output = to_markdown(generate_content(processed_text))
     # return jsonify({'message': 'OK'})
-    return jsonify({'processed_text': processed_text})
+    return jsonify({'keywords': markdown_output})
 
 if __name__ == '__main__':
     app.run(port=8080)
